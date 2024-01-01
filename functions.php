353be545e882b858fdde82f694a507634d40d87b
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-01-01 18:31:24 +0600

    Updated functions.php

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function almumeetu_css_js_file_calling()
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '6.4.2', 'true');
 	wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', 'true');
-}
+} 
 
 add_action('wp_enqueue_scripts', 'almumeetu_css_js_file_calling');
 
